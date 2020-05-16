cc5fd888526b16324907c73b9228a713e8b315b7
Levi D. Smith <me@levidsmith.com>
2020-05-15 21:13:24 -0400

    Database tables update

--- MySQL/create_tables.sql
+++ MySQL/create_tables.sql
@@ -1,10 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.11.3
--- http://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
--- Generation Time: May 02, 2017 at 06:06 PM
+-- Generation Time: May 15, 2020 at 06:08 PM
+-- PHP Version: 7.2.7
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
 
 --
 -- Table structure for table `game`
@@ -13,18 +17,52 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE `game` (
   `id` int(11) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `order_method` int(11) DEFAULT '0',
+  `score_format` varchar(64) DEFAULT NULL,
+  `metric` varchar(256) DEFAULT NULL,
+  `download_url` varchar(256) NOT NULL,
+  `leaderboard_key` varchar(32) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `score`
 --
 
 CREATE TABLE `score` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'Anonymous',
-  `score` int(5) unsigned NOT NULL DEFAULT '0',
+  `score` int(5) UNSIGNED NOT NULL DEFAULT '0',
   `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `game` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game` (`game`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `score`
+--
+ALTER TABLE `score`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=554;
+COMMIT;
+
